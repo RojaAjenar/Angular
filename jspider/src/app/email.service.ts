b8e0ef1dc2f02d4126e5434fdea469039c2f5fa7
RojaAjenar <rojameera98@gmail.com>
2020-01-06 22:40:08 +0530

    msg

---
+++ jspider/src/app/email.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+
+
+export class EmailService{
+    getEmails(){
+        return[
+            "roja@",
+            "ramya",
+            "anu",
+            "djjd"
+        ];
+    }
+}
---
+++ jspider/src/app/username.validators.ts
@@ -0,0 +1,23 @@
+import { AbstractControl, ValidationErrors, } from '@angular/Forms';
+
+
+export class usernamevalidators{
+  
+static  donotGiveSpace(control:AbstractControl):ValidationErrors | null {
+        if((control.value as string).indexOf(" ")>=0){
+             return{ donotGiveSpace:true};
+        }else {
+            return null;
+        }
+}
+static unique(u:AbstractControl):ValidationErrors | null {
+    if(u.value === "roja"){
+        return{unique:true}
+    }
+      else{
+          return null;
+      }
+
+}
+}
+
